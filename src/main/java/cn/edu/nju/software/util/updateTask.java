c68db2c2496979c7a0d7377ff02682f52346eec3
612yese <240590367@qq.com>
2018-04-03 16:06:50 +0800

    entity修改

--- src/main/java/cn/edu/nju/software/util/updateTask.java
+++ src/main/java/cn/edu/nju/software/util/updateTask.java
@@ -1,11 +1,15 @@
 package cn.edu.nju.software.util;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
 import javax.servlet.ServletContext;
 import java.util.Date;
 import java.util.TimerTask;
 
 public class updateTask extends TimerTask{
     private boolean isRunning = false;
+    private ApplicationContext app;
     private ServletContext servletContext = null;
     public updateTask(){}
     public updateTask(ServletContext servletContext){
@@ -13,9 +17,12 @@ public class updateTask extends TimerTask{
     }
     @Override
     public void run() {
+//        可用于获取ApplicationContext,进行后续操作，暂时放着
+//        app = WebApplicationContextUtils.getWebApplicationContext(servletContext);
+//        app.getBean("searchService");
         if (!isRunning) {
             isRunning = true;
-            System.out.println("开始执行任务..."); //开始任务
+            System.out.println("开始执行更新索引库任务..."); //开始任务
             System.out.println(new Date());
             //TODO  需要执行任务的代码
             System.out.println("执行任务完成..."); //任务完成
